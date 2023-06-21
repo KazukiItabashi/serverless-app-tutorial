7cb6ecf9a09f5d4d62fccb5ccec1fe28500dba30
itabashi <k.itabashi@scsk.jp>
2023-06-21 18:34:26 +0900

    Change cognito config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-1_JMzQ5qyEy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5t5qkpscr81hiqo8di2nu8sfa7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://44p3cyakh9.execute-api.ap-northeast-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
